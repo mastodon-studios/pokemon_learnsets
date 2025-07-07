447d3b26adf6a9be0d74b85c38714f792475e36f
Nicholas J. Roberts <njeffrob@gmail.com>
2025-07-07 18:11:54 -0300

    Added a basic data frame and made some notes

--- pokemon_learnsets.R
+++ pokemon_learnsets.R
@@ -1,24 +1,33 @@
 library(shiny)
 library(bslib)
 
+# Some notes: Each gen adds to the last, i.e. gen 1 mons move sets are different than
+# gen 1 mons in gen 2, etc. This is going to be an exponential amount of work
+
+# Looking over some resources, I want data frames and to put them into tables
+# Problem: multirow tables
+
 # Pokemon data
-# Gen 1
+# Gen 1 moves
+gen_1_pokemon <- data.frame(
+    Pokemon = c("Bulbasaur"),
+)
 
-# Gen 2
+# Gen 2 moves
 
-# Gen 3
+# Gen 3 moves
 
-# Gen 4
+# Gen 4 moves
 
-# Gen 5
+# Gen 5 moves
 
-# Gen 6
+# Gen 6 moves
 
-# Gen 7
+# Gen 7 moves
 
-# Gen 8
+# Gen 8 moves
 
-# Gen 9
+# Gen 9 moves
 
 # User interface
 
