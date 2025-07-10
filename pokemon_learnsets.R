0b24d1ca3a4a0050130fc1be4a4ac49daa3ee2df
Nicholas J. Roberts <njeffrob@gmail.com>
2025-07-10 16:57:42 -0300

    made some progress on the data tables.

--- pokemon_learnsets.R
+++ pokemon_learnsets.R
@@ -1,6 +1,7 @@
 library(shiny)
-library(flextable)
 library(bslib)
+library(flextable)
+library(DT)
 
 # Some notes: Each gen adds to the last, i.e. gen 1 mons move sets are different than
 # gen 1 mons in gen 2, etc. This is going to be an exponential amount of work
@@ -8,6 +9,8 @@ library(bslib)
 # Looking over some resources, I want data frames and to put them into tables
 # Problem: multirow tables. Solution: flextable
 # Thing to maybe add later: sprites :)
+
+# Note: flextable doesn't work with Shiny :( but I found a DT solution that looks ok
 
 # WHAT YOU WANT: https://www.youtube.com/watch?v=_as239LdGvY 6:48 in
 # Sample code:
@@ -31,11 +34,21 @@ gen_1_pkm <- data.frame(
     pp = c((1:10), 1)
 )
 
+# keeping these for now as an example
 gen_1_pkm_flex <- flextable(gen_1_pkm)
-vm_gen_1_pkm_flex <- merge_v(gen_1_pkm_flex)
+mv_gen_1_pkm_flex <- merge_v(gen_1_pkm_flex)
 
 
 # Gen 2 moves
+gen_2_pkm <- data.frame(
+    Pokemon = c(replicate(10, "Bulbasaur"), "Squirtle"),
+    Level = c((1:10), 1),
+    Move = c((1:10), 1),
+    Type = c((1:10), 1),
+    Power = c((1:10), 1),
+    Accuracy = c((1:10), 1),
+    pp = c((1:10), 1)
+)
 
 # Gen 3 moves
 
@@ -57,23 +70,43 @@ gen_select <- c(1:9)
 
 # User interface
 ui <- fluidPage(
-    selectInput("dataset", label = "Dataset", choices = ls("package:datasets")),
-    verbatimTextOutput("summary"),
-    tableOutput("table")
+    #textOutput("test"),
+
+    selectInput("generations", "Generation Selector", gen_select),
+    DT::DTOutput("gen1")
 )
 
 
 # Server
 server <- function(input, output, session) {
-    output$summary <- renderPrint({
-        dataset <- get(input$dataset, "package:datasets")
-        summary(dataset)
-    })
+    #output$test <- renderText(
+    #    {
+    #        paste("Current Gen is:", input$generations)
+    #    }
+    #)
 
-    output$table <- renderTable({
-        dataset <- get(input$dataset, "package:datasets")
-        dataset
-    })
+    # I want to conditionally show the table based on the selector input
+    # tackle this another day
+    if (input$generations == 1) {
+        output$gen1 <- DT::renderDT(
+            {
+                dtable <- datatable(
+                    gen_1_pkm,
+                    rownames = FALSE,
+                    options = list(rowsGroup = list(0))
+                )
+                path <- "/home/nick/R/x86_64-pc-linux-gnu-library/4.5/DT"
+                dep <- htmltools::htmlDependency(
+                    "RowsGroup",
+                    "2.0.0",
+                    path,
+                    script = "dataTables.rowsGroup.js"
+                )
+                dtable$dependencies <- c(dtable$dependencies, list(dep))
+                dtable
+            }
+        )
+    } else {}
 }
 
 # calling shinyApp
